0bcafb791bdaf70c860aa2e8641103c1221ea891
calvinho <cho@wee.church>
2022-05-17 21:15:16 -0700

    add android support to receive Send Intent

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,14 +1,14 @@
+export interface ShareExtensionPlugin {
+    checkSendIntentReceived(): Promise<Intent>;
+    finish(): void;
+    saveDataToKeychain(options: { key: string, data: any }): Promise<string>;
+    clearKeychainData(options: { key: string }): Promise<any>;
+}
+
 export interface Intent {
     title?: string;
     description?: string;
     type?: string;
     url?: string;
     additionalItems?: any;
-}
-
-export interface ShareExtensionPlugin {
-    checkSendIntentReceived(): Promise<Intent>;
-    finish(): void;
-    saveDataToKeychain(options: { key: string, data: any }): Promise<string>;
-    clearKeychainData(options: { key: string }): Promise<any>;
 }
