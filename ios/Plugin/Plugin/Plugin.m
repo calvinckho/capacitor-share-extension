cd0c924fdec07d114f4ac3e6007db46125ff5c95
calvinho <cho@wee.church>
2022-05-18 22:36:07 -0700

    define new plugin APIs

--- ios/Plugin/Plugin/Plugin.m
+++ ios/Plugin/Plugin/Plugin.m
@@ -4,6 +4,8 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(ShareExtension, "ShareExtension",
+           CAP_PLUGIN_METHOD(checkSendIntentReceived, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(finish, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(saveDataToKeychain, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(clearKeychainData, CAPPluginReturnPromise);
 )
